e9dc4da69596018bbdda400a235cb8e59e8a3ec1
Griczzi <Griczi@mail.ru>
2020-04-06 23:29:03 +0300

    fixed all style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Header from "./header"
-
+import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header style={{position: `fixed`, top: 0}} />
-      <div style={{ margin: `0 auto`, maxWidth: 1920, position: `relative`}}>
+      <div className="layout_wrapper">
         <main>{children}</main>
       </div>
     </>
